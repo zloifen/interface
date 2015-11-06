0201cd85ef345225ed7b7c67e3f50201a6d9d767
zloifen <al.qwertyas@yandex.ru>
2015-11-06 12:03:18 +0300

    88005553535

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ echo "<!DOCTYPE html>
         <span class='icon-bar'></span>
     </button>
     <div class='navbar-header'>
-        <a class='navbar-brand' href='/'>Больница №88005553535</a>
+        <a class='navbar-brand' href='/'>Больница №9</a>
     </div>
     <div class='navbar-collapse collapse navbar-responsive-collapse'>
         <ul class='nav navbar-nav'>
